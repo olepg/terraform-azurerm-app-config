5338e5b2074d49c6c089a1ed6db6467be4475160
ole.gjose <ogjo@equinor.com>
2025-12-01 13:04:13 +0100

    feat: set prevent_destroy to false for App Configuration store lifecycle

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "azurerm_app_configuration" "this" {
 
   lifecycle {
     # Prevent accidental destroy of App Configuration store.
-    prevent_destroy = var.prevent_destroy
+    prevent_destroy = false
   }
 }
 
